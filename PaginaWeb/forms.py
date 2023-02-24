a2a1cade8b650e64bfc1a8d200cb4a5cdd8a2d38
Oscar <oschneider742@gmail.com>
2023-02-24 01:14:37 -0300

    sese

--- PaginaWeb/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class FormularioContacto(forms.Form):
-    nombre = forms.CharField()
-    mail = forms.EmailField()
-    asunto = forms.CharField()
-    mensaje = forms.CharField(widget=forms.Textarea())
