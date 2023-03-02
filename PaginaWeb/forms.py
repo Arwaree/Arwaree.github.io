ec7334e59249e798bf16ee300c4808e0ab1942ad
Oscar <oschneider742@gmail.com>
2023-03-02 15:39:09 -0300

    envio de mails

--- PaginaWeb/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class FormularioContacto(forms.Form):
-    nombre = forms.CharField()
-    mail = forms.EmailField(label='Su Mail')
-    asunto = forms.CharField()
-    mensaje = forms.CharField(widget=forms.Textarea())
--- PaginaWeb/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from PaginaWeb.forms import *
-
-# Create your views here.
-
-def index(request):
-
-    formulario = FormularioContacto()
-    return render(request, 'index.html',{'form':formulario})
